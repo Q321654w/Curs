1c9a8ca5fd3e36979609833c249d1faba8c323c5
Q321654w <Q321654w@gmail.com>
2024-12-15 22:22:23 +0300

    Fix

--- Assets/Features/V2/Vehicle.cs
+++ Assets/Features/V2/Vehicle.cs
@@ -19,16 +19,12 @@ namespace V2
         {
             for (int i = 0; i < Wheels.Length; i++)
             {
-                var collider = Wheels[i].Collider;
-                var steerAngle = collider.steerAngle;
-                var motorTorque = collider.motorTorque;
-                
                 if (i < 2)
                 {
-                    collider.steerAngle = Mathf.Clamp(steerAngle + steerInput, -Motor.MaxSteerAngle, Motor.MaxSteerAngle) ;
+                    Wheels[i].Collider.steerAngle = steerInput;
                 }
 
-                collider.motorTorque = Mathf.Clamp(motorTorque + motorInput, -Motor.MaxTorque, Motor.MaxTorque);
+                Wheels[i].Collider.motorTorque = motorInput;
             }
         }
     }
